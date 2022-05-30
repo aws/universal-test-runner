e3c9b9fe55ad8eb01f8f3b41979dad01416b878b
Michael Rose <msrose@amazon.com>
2022-05-30 12:17:39 -0400

    Add node setup script

---
+++ setup-node.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Used to install the right Node.js (and NPM) version when running in CODE.AWS
+# When installing Node.js in your local dev env, use the instructions in the
+# NVM README: https://github.com/nvm-sh/nvm#installing-and-updating
+
+echo "Installing nvm..."
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | PROFILE=/dev/null bash
+
+echo "Initializing nvm..."
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use  # This loads nvm
+
+echo "Installing Node.js..."
+
+nvm install
+
+echo "Done."
