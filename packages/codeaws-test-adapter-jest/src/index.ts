f81f3476c52ed2ee8d9687fcf0a80c8d2bd7a3e5
Michael Rose <msrose@amazon.com>
2022-06-20 17:53:09 -0400

    Add simple command detection to jest adapter

--- packages/codeaws-test-adapter-jest/src/index.ts
+++ packages/codeaws-test-adapter-jest/src/index.ts
@@ -1,22 +1,101 @@
 import path from 'path'
 import { spawnSync } from 'child_process'
-import { accessSync, constants } from 'fs'
+import { access, readFile } from 'fs/promises'
 import log from './log'
 
 import { AdapterInput, AdapterOutput } from '@sentinel-internal/codeaws-test-runner'
 
-export function executeTests({ testNamesToRun = [] }: AdapterInput): AdapterOutput {
-  const executable = path.resolve('.', 'node_modules', '.bin', 'jest')
-  try {
-    accessSync(executable, constants.X_OK)
-  } catch (e) {
-    throw new Error(`Could not find jest executable! Checked ${executable}`)
-  }
-  const args = []
+/**
+ * Start by determining what executable to use, then what args to pass to the executable.
+ *
+ * Executable:
+ * - If the user has defined a package.json that we detect as executing jest in some way:
+ *   - Assume that we'll run tests via their package manager
+ *   - If a yarn.lock file is present, use `yarn` as our executable. Otherwise use `npm`
+ * - Otherwise, use ./node_modules/.bin/jest as the executable if present
+ * - Otherwise, use global jest
+ *
+ * Args:
+ * - If we are invoking via package manager, use the script name as the first arg
+ * - Add any jest-specific args required to run the tests according to what's passed by the runner
+ */
+export async function executeTests({ testNamesToRun = [] }: AdapterInput): Promise<AdapterOutput> {
+  const [executable, args] = await buildBaseJestTestCommand()
   if (testNamesToRun.length > 0) {
-    args.push('-t', testNamesToRun.join('|'))
+    args.push('--testNamePattern', testNamesToRun.join('|'))
   }
-  log.stderr(`Invoking jest with command: ${[executable, ...args].join(' ')}`)
+  log.stderr(`Running tests with jest using command: ${[executable, ...args].join(' ')}`)
   const { status } = spawnSync(executable, args, { stdio: 'inherit' })
   return { exitCode: status ?? 1 }
+}
+
+async function buildBaseJestTestCommand(): Promise<[string, string[]]> {
+  try {
+    const packageJsonContents = await loadPackageJson()
+    const packageJson = parsePackageJson(packageJsonContents)
+    const testScriptName = pickTestScript(packageJson)
+    return await buildPackageManagerTestCommand(testScriptName)
+  } catch (e: any) {
+    log.stderr(e.message)
+    const jestExecutablePath = path.join('.', 'node_modules', '.bin', 'jest')
+    const executable = (await exists(jestExecutablePath)) ? jestExecutablePath : 'jest'
+    return [executable, []]
+  }
+}
+
+async function loadPackageJson(): Promise<string> {
+  try {
+    return await readFile(path.resolve('.', 'package.json'), 'utf-8')
+  } catch (e) {
+    throw new Error('Unable to load package.json')
+  }
+}
+
+interface PackageJson {
+  scripts?: { [key: string]: any }
+}
+
+function parsePackageJson(contents: string): PackageJson {
+  try {
+    return JSON.parse(contents)
+  } catch (e) {
+    throw new Error('Unable to parse package.json contents')
+  }
+}
+
+function pickTestScript(packageJson: PackageJson): string {
+  const { scripts = {} } = packageJson
+  const testScripts = Object.entries(scripts).filter(([, script]: [string, any]) => {
+    const tokens = new Set(String(script).split(' '))
+    return tokens.has('jest') || tokens.has(path.join('node_modules', '.bin', 'jest'))
+  })
+  const scriptNames = testScripts.map(([scriptName]) => scriptName).join(', ')
+  log.stderr(`Found ${testScripts.length} possible test script(s) in package.json: ${scriptNames}`)
+  const testScript =
+    testScripts.find(([scriptName]: [string, any]) => scriptName === 'test') || testScripts[0]
+  if (!testScript) {
+    throw new Error('Unable to identify any test scripts in package.json')
+  }
+  const [scriptName] = testScript
+  log.stderr(`Picking script "${scriptName}" from package.json`)
+  return scriptName
+}
+
+async function buildPackageManagerTestCommand(testScriptName: string): Promise<[string, string[]]> {
+  // TODO: check if executables actually exist
+  if (await exists(path.resolve('yarn.lock'))) {
+    log.stderr(`Found yarn.lock file, invoking script "${testScriptName}" using yarn...`)
+    return ['yarn', [testScriptName]]
+  }
+  log.stderr(`Invoking script "${testScriptName}" using npm...`)
+  return ['npm', ['run', testScriptName, '--']]
+}
+
+async function exists(path: string): Promise<boolean> {
+  try {
+    await access(path)
+    return true
+  } catch (e) {
+    return false
+  }
 }
