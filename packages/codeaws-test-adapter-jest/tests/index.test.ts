9a3ee82b32f3595eb106824540e80e2469d3cf68
Michael Rose <msrose@amazon.com>
2022-06-22 12:28:49 -0400

    Add simple tests for jest adapter

--- packages/codeaws-test-adapter-jest/tests/index.test.ts
+++ packages/codeaws-test-adapter-jest/tests/index.test.ts
@@ -1,5 +1,20 @@
+jest.mock('../src/log')
+
 describe('Jest adapter', () => {
-  it('has some tests', () => {
-    expect(1 + 1).toBe(2)
+  it('executes jest when given tests to run', async () => {
+    jest.doMock('../src/buildBaseTestCommand', () => () => ['./node_modules/.bin/jest', []])
+
+    const runCommand = jest.fn(() => ({ status: 0 }))
+    jest.doMock('../src/runCommand', () => runCommand)
+
+    const { executeTests } = await import('../src/index')
+
+    const { exitCode } = await executeTests({ testNamesToRun: ['bill', 'bob', 'mary'] })
+
+    expect(exitCode).toBe(0)
+    expect(runCommand).toHaveBeenCalledWith('./node_modules/.bin/jest', [
+      '--testNamePattern',
+      'bill|bob|mary',
+    ])
   })
 })
