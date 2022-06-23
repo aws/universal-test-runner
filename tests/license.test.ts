f7514563d51b3b4ba9a2b3f2c0ee6b6e62f5f1fa
Michael Rose <msrose@amazon.com>
2022-06-23 12:47:46 -0400

    Add some more license tests

--- tests/license.test.ts
+++ tests/license.test.ts
@@ -6,11 +6,29 @@ import path from 'path'
 
 const LICENSE_CONTENTS = fs.readFileSync(path.join(__dirname, '..', 'LICENSE'), 'utf-8')
 const PACKAGES_DIR = path.join(__dirname, '..', 'packages')
+const PACKAGE_DIRS = fs.readdirSync(PACKAGES_DIR)
 
 describe('License file', () => {
-  it.each(fs.readdirSync(PACKAGES_DIR))('is included in the root of %s', (packageRoot) => {
-    expect(fs.readFileSync(path.join(PACKAGES_DIR, packageRoot, 'LICENSE'), 'utf-8')).toBe(
-      LICENSE_CONTENTS,
+  it.each(PACKAGE_DIRS)('is included in the root of %s', (packageRoot) => {
+    const packageLicenseContents = fs.readFileSync(
+      path.join(PACKAGES_DIR, packageRoot, 'LICENSE'),
+      'utf-8',
     )
+    expect(packageLicenseContents).toBe(LICENSE_CONTENTS)
+  })
+})
+
+describe('License identifier', () => {
+  it.each(PACKAGE_DIRS)(
+    'is included in the package.json license field for %s',
+    async (packageRoot) => {
+      const packageJson = await import(path.join(PACKAGES_DIR, packageRoot, 'package.json'))
+      expect(packageJson.license).toBe('Apache-2.0')
+    },
+  )
+
+  it('is included in the root package.json license field', async () => {
+    const packageJson = await import(path.join(__dirname, '..', 'package.json'))
+    expect(packageJson.license).toBe('Apache-2.0')
   })
 })
