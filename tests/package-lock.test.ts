7b920bf3fb0c45109b97af68c9e8597e8cdec8d8
Michael Rose <msrose@amazon.com>
2022-06-23 14:18:47 -0400

    Add test for codeartifact URLs in package-lock

---
+++ tests/package-lock.test.ts
@@ -0,0 +1,12 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import fs from 'fs'
+import path from 'path'
+
+describe('package-lock.json', () => {
+  it('does not include any codeartifact URLs', async () => {
+    const packageLockJson = fs.readFileSync(path.join(__dirname, '..', 'package-lock.json'))
+    expect(packageLockJson).not.toContain('codeartifact')
+  })
+})
