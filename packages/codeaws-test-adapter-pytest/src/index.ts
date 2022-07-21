f5523e32a2112285956f9f4c3bbc978680f4f02b
Michael Rose <msrose@amazon.com>
2022-07-21 17:57:49 -0400

    Add pytest adapter

---
+++ packages/codeaws-test-adapter-pytest/src/index.ts
@@ -0,0 +1,23 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import runCommand from './runCommand'
+import log from './log'
+
+import { AdapterInput, AdapterOutput } from '@sentinel-internal/codeaws-test-runner'
+
+export async function executeTests({ testNamesToRun = [] }: AdapterInput): Promise<AdapterOutput> {
+  const executable = 'pytest'
+  const args = []
+  if (testNamesToRun.length > 0) {
+    args.push('-k', `${testNamesToRun.join(' or ')}`)
+  }
+
+  // spawnSync will automatically quote any args with spaces in them, so we don't need to
+  // manually include the quotes when running the command. Here we do it for display purposes.
+  const quotedArgs = args.map((arg) => (arg.includes(' ') ? `'${arg}'` : arg))
+  log.stderr(`Running tests with pytest using command: ${[executable, ...quotedArgs].join(' ')}`)
+
+  const { status } = await runCommand(executable, args)
+  return { exitCode: status ?? 1 }
+}
