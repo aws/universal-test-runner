5f7d3eb3b5bad236e71e6e51312479fddf2b4fcb
Michael Rose <msrose@amazon.com>
2022-09-16 16:40:07 -0400

    chore: Add commitlint (#46)

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [0],
+  },
+}
