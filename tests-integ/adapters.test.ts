f4f146d34ee7f4401004fbe77ab3181902da35e7
Michael Rose <msrose@amazon.com>
2022-09-21 18:28:38 -0400

    test: Add integ tests for maven and gradle (#53)

--- tests-integ/adapters.test.ts
+++ tests-integ/adapters.test.ts
@@ -5,6 +5,10 @@ import { runSetupScript, runCli, parseJunitReport, remove } from './helpers'
 
 const ADAPTERS = ['jest', 'pytest']
 
+// Still have to figure out reporting for maven and gradle
+// For now just run integ tests just with assertions on exit status
+const ADAPTERS_WITH_PENDING_REPORT_SUPPORT = ['maven', 'gradle']
+
 describe.each(ADAPTERS)('%s adapter', (adapter) => {
   beforeAll(() => {
     runSetupScript(adapter)
@@ -35,5 +39,26 @@ describe.each(ADAPTERS)('%s adapter', (adapter) => {
     })
     const report = parseJunitReport(adapter, 'reports/report.xml')
     expect(report).toMatchSnapshot()
+  })
+})
+
+describe.each(ADAPTERS_WITH_PENDING_REPORT_SUPPORT)('%s adapter', (adapter) => {
+  beforeAll(() => {
+    runSetupScript(adapter)
+  })
+
+  it('runs all tests', () => {
+    const { status } = runCli(adapter, {
+      TEP_VERSION: '0.1.0',
+    })
+    expect(status).toBe(0)
+  })
+
+  it('runs a subset of tests', () => {
+    const { status } = runCli(adapter, {
+      TEP_VERSION: '0.1.0',
+      TEP_TESTS_TO_RUN: 'test1|test2',
+    })
+    expect(status).toBe(0)
   })
 })
