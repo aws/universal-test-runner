6317b6ed5258e471b5e8398ac689ad1a6a8a19c0
Michael Rose <msrose@amazon.com>
2022-09-22 17:06:40 -0400

    build: Add logging for CI debugging

--- scripts/packages.ts
+++ scripts/packages.ts
@@ -1,12 +1,18 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+/* eslint-disable no-console */
+
 import fs from 'fs'
 import path from 'path'
 
 const PACKAGES_DIR = path.join(__dirname, '..', 'packages')
 
-export const packages = fs.readdirSync(PACKAGES_DIR).map((packagePath) => {
+const dirContents = fs.readdirSync(PACKAGES_DIR)
+
+console.log('Read the following from packages dir:', dirContents)
+
+export const packages = dirContents.map((packagePath) => {
   const packageRoot = path.join(PACKAGES_DIR, packagePath)
   const packageJson = JSON.parse(fs.readFileSync(path.join(packageRoot, 'package.json'), 'utf-8'))
   return {
