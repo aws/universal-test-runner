d3c7fdbf6490b606fc46a1578ac1268bceef414d
Adam Khamis <khamisad@amazon.com>
2022-12-13 13:26:32 -0500

    feat: gradle filepath and test suite support

--- packages/universal-test-adapter-gradle/src/index.ts
+++ packages/universal-test-adapter-gradle/src/index.ts
@@ -7,10 +7,9 @@ import { log } from './log'
 import { AdapterInput, AdapterOutput } from '@sentinel-internal/universal-test-runner-types'
 
 // Transforms filepath input from 'folderA/folderB/file.java)' to 'folderA.folderB'
-export const parsePackagePath = (filepath: String): String => {
-  return filepath.substring(0, filepath.lastIndexOf('/')).replace('/', '.');
+export const parsePackagePath = (filepath: string): string => {
+  return filepath.substring(0, filepath.lastIndexOf('/')).replace('/', '.')
 }
-
 
 export async function executeTests({ testsToRun = [] }: AdapterInput): Promise<AdapterOutput> {
   const executable = 'gradle'
@@ -18,11 +17,11 @@ export async function executeTests({ testsToRun = [] }: AdapterInput): Promise<A
   const testNamesToRun = testsToRun.map(({ testName, suiteName, filepath }) => {
     // *packagePath.suiteName.testName
     return `*${filepath ? parsePackagePath(filepath) : ''}.${suiteName || '*'}.${testName}`
-  }
-      
-  )
+  })
   if (testNamesToRun.length > 0) {
-    args.push(...testNamesToRun.flatMap((fullyQualifiedName) => ['--tests', `${fullyQualifiedName}`]))
+    args.push(
+      ...testNamesToRun.flatMap((fullyQualifiedName) => ['--tests', `${fullyQualifiedName}`]),
+    )
   }
   log.info(`Running tests with gradle using command: ${[executable, ...args].join(' ')}`)
   const { status } = await runCommand(executable, args)
