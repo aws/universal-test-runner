031b2ce57dda6c7a33c20f0cd1d35a1671acdc7d
Michael Rose <msrose@amazon.com>
2023-01-06 14:26:46 -0500

    ci: Disable tolerable errors

--- packages/universal-test-adapter-gradle/src/runCommand.ts
+++ packages/universal-test-adapter-gradle/src/runCommand.ts
@@ -10,6 +10,7 @@ interface CommandResult {
 
 // Return an promise since we're likely to change from spawnSync to spawn (or something else async) at some point
 export function runCommand(executable: string, args: string[]): Promise<CommandResult> {
+  // nosemgrep: javascript.lang.security.detect-child-process.detect-child-process
   const { status, error } = spawnSync(executable, args, { stdio: 'inherit' })
 
   return Promise.resolve({ status, error })
