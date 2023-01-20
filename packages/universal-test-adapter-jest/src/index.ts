77a3b2e62aa830029011e5fb047e01e6b370c195
Adam Khamis <khamisad@amazon.com>
2023-01-20 15:25:48 -0500

    feat: jest file and suite name filtering

--- packages/universal-test-adapter-jest/src/index.ts
+++ packages/universal-test-adapter-jest/src/index.ts
@@ -7,13 +7,70 @@ import { buildBaseTestCommand } from './buildBaseTestCommand'
 
 import { AdapterInput, AdapterOutput } from '@aws/universal-test-runner-types'
 
+// could also use https://www.npmjs.com/package/path-to-glob-pattern?activeTab=explore
+// Examples:
+// src/dirA/dirB/file.js -> **/src/dirA/dirB/file.js
+// src\dirA\.*\dirB -> **/src/dirA/**/dirB/** 
+const pathToGlob = (filepath: string): string => {
+  //should do this different to avoid 2 linear searches
+  filepath = filepath.replace(/\\/g, '/').replace(/\.\*/g, '**')
+
+  filepath = !filepath.startsWith('**') ? 
+  (filepath.startsWith('/') ? '**' + filepath : '**/' + filepath) 
+  : filepath
+
+  // no file specified, only dirs
+  if (!filepath.includes('.')) {
+    if (!filepath.endsWith('**') ) {
+      // src/dirA/dirB
+      if (!filepath.endsWith('/')) {
+        filepath = filepath + '/**'
+      }
+      // src/dirA/dirB/
+      else {
+        filepath = filepath + '**'
+      }
+    }
+  }
+  return filepath
+}
+
 export async function executeTests({ testsToRun = [] }: AdapterInput): Promise<AdapterOutput> {
   const [executable, args] = await buildBaseTestCommand()
+  // const executable = 'jest'
+  // const args = []
 
-  const testNamesToRun = testsToRun.map(({ testName }) => testName)
+  const filepaths: string[] = []
+  const describeIts: string[] = []
 
-  if (testNamesToRun.length > 0) {
-    args.push('--testNamePattern', testNamesToRun.join('|'))
+  var filepathWithTestOrSuiteCount = 0;
+
+  const testNamesToRun = testsToRun.forEach(({ testName, suiteName, filepath }) => {
+    if (filepath && (testName || suiteName)) {
+      filepathWithTestOrSuiteCount++
+    }
+    filepath && filepaths.push(`(${pathToGlob(filepath)})`)
+    if (suiteName && testName) {
+      describeIts.push(`(${suiteName} ${testName})`)
+    }
+    else {
+      suiteName ? describeIts.push(`(${suiteName})`) : describeIts.push(`(${testName})`)
+    }
+  })
+
+  // since we can't map test names/suites to a filepath for jest, we cannot include filepaths in this call
+  if (filepaths.length > 0) {
+    if (describeIts.length == 0 || filepathWithTestOrSuiteCount == describeIts.length) {
+      args.push('--testMatch', `${filepaths.join('|')}`)
+    }
+    else if (filepathWithTestOrSuiteCount != describeIts.length) {
+      log.warn('Detected entry that includes a filepath but does not include a test suite/name! ' +
+      'This is not supported since jest cannot map files to tests as input, ' +
+      'please either remove all references to filepaths or include it in all entries')
+    }
+  }
+  if (describeIts.length > 0) {
+    args.push('--testNamePattern', `${describeIts.join('|')}`)
   }
 
   log.info(`Running tests with jest using command: ${[executable, ...args].join(' ')}`)
--- packages/universal-test-adapter-jest/tests/index.test.ts
+++ packages/universal-test-adapter-jest/tests/index.test.ts
@@ -4,6 +4,15 @@
 jest.mock('../src/log')
 
 describe('Jest adapter', () => {
+  let spawn: any
+
+  beforeEach(() => {
+    spawn = jest.fn(() => ({ status: 0 }))
+
+    jest.resetModules()
+    jest.doMock('@aws/universal-test-runner-spawn', () => ({ spawn }))
+  })
+
   it('executes jest when given tests to run', async () => {
     jest.doMock('../src/buildBaseTestCommand', () => ({
       buildBaseTestCommand: () => ['./node_modules/.bin/jest', []],
@@ -21,7 +30,51 @@ describe('Jest adapter', () => {
     expect(exitCode).toBe(0)
     expect(spawn).toHaveBeenCalledWith('./node_modules/.bin/jest', [
       '--testNamePattern',
-      'bill|bob|mary',
+      '\'(bill)|(bob)|(mary)\'',
     ])
   })
+
+  it('excludes filepaths when not included in every entry', async () => {
+    const { executeTests } = await import('../src/index')
+
+    const { exitCode } = await executeTests({
+      testsToRun: [
+        { testName: 'bill' },
+        { filepath: 'package/fileB.ext', testName: 'bob' },
+        { suiteName: 'suiteC', testName: 'mary' },
+      ],
+    })
+
+    expect(exitCode).toBe(0)
+    expect(spawn).toHaveBeenCalledWith('./node_modules/.bin/jest', [
+      '--testNamePattern',
+      '\'(bill)|(bob)|(suiteC mary)\'',
+    ])
+  })
+
+  it('converts filepaths to globs correctly', async () => {
+    const { executeTests } = await import('../src/index')
+
+    const { exitCode } = await executeTests({
+      testsToRun: [
+        { filepath: 'package/dirA',  testName: 'bob' },
+        { filepath: 'package/fileB.ext', testName: 'bob' },
+        { filepath: '.*/package/.*/dirB/.*',  testName: 'bob', suiteName: 'w' },
+        { filepath: '/package/.*/dirB',  testName: 'bob' },
+        { filepath: '\\package\\dirA',  testName: 'bob' },
+
+      ],
+    })
+
+    expect(exitCode).toBe(0)
+    expect(spawn).toHaveBeenCalledWith('./node_modules/.bin/jest', [
+      '--testMatch',
+      '\'**/package/dirA/** **/package/fileB.ext **/package/**/dirB/** **/package/**/dirB/** **/package/dirA/**\'',
+      '--testNamePattern',
+      '\'(bob)|(bob)|(w bob)|(bob)|(bob)\'',
+    ])
+  })
+
+
+
 })
