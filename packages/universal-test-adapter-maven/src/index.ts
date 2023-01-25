63d33d513160c1b1da8fc0914cc05e1530bc1495
Adam Khamis <khamisad@amazon.com>
2023-01-25 10:47:15 -0500

    feat: add mvn adapter filtering

--- packages/universal-test-adapter-maven/src/index.ts
+++ packages/universal-test-adapter-maven/src/index.ts
@@ -6,35 +6,33 @@ import { log } from './log'
 
 import { AdapterInput, AdapterOutput } from '@aws/universal-test-runner-types'
 
+import path from 'path'
+
+// Duplicated code, need to move into a utils packages
+const toUnixPath = (filepath: string): string => {
+  // https://quickref.me/convert-a-windows-file-path-to-unix-path
+  return filepath.replace(/[\\/]+/g, '/').replace(/^([a-zA-Z]+:|\.\/)/, '')
+}
+
 // mvn expects the format $PATH_TO_FILE/$CLASS_NAME#$TEST_NAME
-// will fail to find the test if suiteName is not passed, or if filepath does not include the name of the file (can't pass a subdir that does not include the filename)
 export const parseFilepathAndClassName = (
   filepath: string | undefined,
   suiteName: string | undefined,
 ): string => {
   if (!filepath) {
-    return ''
+    return suiteName || ''
   }
 
-  let newFilepath = filepath
-
-  if (!suiteName) {
-    const fileExtensionIndex = filepath.lastIndexOf('.')
-    if (fileExtensionIndex != -1) {
-      newFilepath = filepath.substring(0, filepath.indexOf('.'))
-    }
-  } else {
-    const indexOfSuiteName = filepath.indexOf(suiteName)
-    if (indexOfSuiteName != -1) {
-      newFilepath = filepath.substring(0, indexOfSuiteName)
-    } else {
-      if (!newFilepath.endsWith('/') || !newFilepath.endsWith('\\')) {
-        newFilepath = newFilepath + '/'
-      }
+  const parsedPath = path.parse(filepath)
+  console.log(JSON.stringify(parsedPath))
+  filepath = `${parsedPath.dir ? parsedPath.dir + '/' : ''}${parsedPath.name}`
+  if (suiteName) {
+    if (parsedPath.name != suiteName) {
+      filepath = filepath + '/' + suiteName
     }
   }
 
-  return newFilepath.replace(/\\/g, '/')
+  return toUnixPath(filepath)
 }
 
 export async function executeTests({ testsToRun = [] }: AdapterInput): Promise<AdapterOutput> {
@@ -46,7 +44,7 @@ export async function executeTests({ testsToRun = [] }: AdapterInput): Promise<A
       `-Dtest=${testsToRun
         .map(({ testName, suiteName, filepath }) => {
           const parsedFilepath = parseFilepathAndClassName(filepath, suiteName)
-          return parsedFilepath + (suiteName ? suiteName + '#' : '#') + testName
+          return parsedFilepath + '#' + testName
         })
         .join(',')}`,
     )
