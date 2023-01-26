b915255b5cf9ecee0acff6cc4d9642f3b02c12b6
Michael Rose <msrose@amazon.com>
2023-01-26 14:46:54 -0500

    build: Add publish script

---
+++ scripts/publish-packages.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+[[ -n "$(git status --porcelain)" ]] && echo "Cannot publish with uncommitted changes!" && exit 1
+
+[[ "$(git branch --show-current)" != "main" ]] && echo "Must be on main branch to publish!" && exit 1
+
+git fetch --tags origin
+
+[[ "$(git rev-parse main)" != "$(git rev-parse origin/main)" ]] \
+  && echo "Current main branch commit does not match origin/main! Run 'git pull origin main' to update." && exit 1
+
+# TODO: verify that last build on GitHub succeeded
+
+npm run clean
+npm run compile
+# npm run bump:release
+
+# npm login
+
+npm publish --workspaces --dry-run --access public
