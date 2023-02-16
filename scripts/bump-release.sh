e51445cf160d1bc945b506d0f2d0c2d52b14da1a
Michael Rose <msrose@amazon.com>
2023-02-16 17:35:06 -0500

    build: Fix bump script

--- scripts/bump-release.sh
+++ scripts/bump-release.sh
@@ -8,11 +8,15 @@ set -e
 
 echo "Bumping all packages with increment $WHAT_BUMP..."
 
-npm version $WHAT_BUMP --workspaces --no-workspaces-update --include-workspace-root --no-git-tag-version
+npm version $WHAT_BUMP --workspaces --include-workspace-root --no-git-tag-version
 
 npx ts-node ./scripts/update-dependency-versions
 
 # Make sure to update the package-lock after all the package versions changed
+npm install --userconfig=/dev/null
+# Have to run it twice because npm is whack and will update the package-lock
+# to contain the old versions of the packages from the registry.
+# Another install fixes that ¯\_(ツ)_/¯
 npm install --userconfig=/dev/null
 
 NEW_VERSION=$(node -e "console.log(require('./package.json').version)")
