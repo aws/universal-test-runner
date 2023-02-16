ec577bd3a9e58a79f3f55bfbaefac20155b379be
Michael Rose <msrose@amazon.com>
2023-02-16 16:42:48 -0500

    refactor: Use memfs in more places instead of ad-hoc mocks

--- packages/universal-test-adapter-jest/src/buildBaseTestCommand.ts
+++ packages/universal-test-adapter-jest/src/buildBaseTestCommand.ts
@@ -2,21 +2,21 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import path from 'path'
-import { access as fsAccess } from 'fs/promises'
+import { promises as fs } from 'fs'
 
 /*
  * - Use ./node_modules/.bin/jest as the executable if present
  * - Otherwise, use global jest
  */
-export async function buildBaseTestCommand(access = fsAccess): Promise<[string, string[]]> {
+export async function buildBaseTestCommand(): Promise<[string, string[]]> {
   const jestExecutablePath = path.join('.', 'node_modules', '.bin', 'jest')
-  const executable = (await exists(jestExecutablePath, access)) ? jestExecutablePath : 'jest'
+  const executable = (await exists(jestExecutablePath)) ? jestExecutablePath : 'jest'
   return [executable, []]
 }
 
-async function exists(path: string, access: typeof fsAccess): Promise<boolean> {
+async function exists(path: string): Promise<boolean> {
   try {
-    await access(path)
+    await fs.access(path)
     return true
   } catch (e) {
     return false
